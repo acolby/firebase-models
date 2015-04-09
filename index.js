1249cce4c9395e2737435f4a19139fc0ebbde0a3
ac_airbear <acolby@live.com>
2015-04-09 11:20:05 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,216 @@
+var factories = require("./lib/factories.js");
+var Firebase = require("firebase");
+var q = require("Q");
+
+module.exports = function(s, firebaseUrl, firebaseSecret) {
+
+	var schema = clone(s);
+
+	// VALIDATE SCHEMA
+	// -- for each model check that the references are valid
+	for (var k in schema) {
+		schema[k].__ref__ = {};
+		schema[k].__refs__ = {};
+		schema[k].__items__ = {};
+		for (var j in schema[k]) {
+			// is id_
+			var key;
+			var error;
+			if (/id_/.test(j)) {
+				key = j.substring(3, j.length);
+				// check if the key exists
+				if (schema[key] === undefined) {
+					error = k + " contains reference " + key + " which does not exist in schema";
+					throw error;
+				} else {
+					schema[k].__ref__[key] = true;
+				}
+			} else if (/ids_/.test(j)) {
+				key = j.substring(4, j.length);
+				// check if the key exists
+				if (schema[key] === undefined) {
+					error = k + " contains reference " + key + " which does not exist in schema";
+					throw error;
+				} else {
+					schema[k].__refs__[key] = true;
+				}
+			} else {
+				key = j;
+				if(!/__/.test(j)){
+					schema[k].__items__[key] = true;
+				}
+			}
+		}
+	}
+
+	var rootRef = new Firebase(firebaseUrl);
+
+	var SchemaModel = {};
+
+	for (var model in schema) {
+		addModelToSchemaObject(model);
+	}
+
+	return SchemaModel;
+
+	function addModelToSchemaObject(model) {
+
+		var modelRef = rootRef.child(model);
+
+		var doesExist = factories.doesExistFactory(modelRef, '*');
+		var add = factories.writeValueToLocationFactory(modelRef, '*');
+		var push = factories.pushValueToLocationFactory(modelRef);
+		var update = factories.writeValueToLocationFactory(modelRef, '*');
+		var setReference = factories.writeValueToLocationFactory(modelRef, '*/*');
+		var addReference = factories.pushValueToLocationFactory(modelRef, '*/*/*');
+		var removeReference = factories.removeValueAtLocationFactory(modelRef, '*/*/*');
+
+		// add base methods
+		SchemaModel[model] = {
+			'doesExist': function(id) {
+				return doesExist(id);
+			},
+			'add': function(id, obj) {
+				for (var item in obj) {
+					if (schema[model].__items__[item] === undefined) {
+						// return rejected promise with error
+						return rejectedPromise({
+							'message': model + ' does not contain ' + item
+						});
+					}
+				}
+				obj.id = id;
+				obj.created = Firebase.ServerValue.TIMESTAMP;
+				return add(obj, id);
+			},
+			'push': function(obj) {
+				for (var item in obj) {
+					if (schema[model].__items__[item] === undefined) {
+						// return rejected promise with error
+						return rejectedPromise({
+							'message': model + ' does not contain ' + item
+						});
+					}
+				}
+				obj.created = Firebase.ServerValue.TIMESTAMP;
+				return push(obj);
+			},
+			'update': function(id, obj) {
+				for (var item in obj) {
+					if (schema[model].__items__[item] === undefined) {
+						// return rejected promise with error
+						return rejectedPromise({
+							'message': model + ' does not contain ' + item
+						});
+					}
+				}
+				return this.doesExist(id)
+				.then(function(exists){
+					if(exists){
+						return update(obj, id);
+					}else{
+						throw({'message': 'id does not exist in ' + model});
+					}
+				});
+			},
+			'setReference': function(id, referenceName, referenceValue) {
+				for (var item in schema[model].__ref__) {
+					var hasRef = false;
+					if (item === referenceName) {
+						hasRef = true;
+					}
+					if(!hasRef){
+						return rejectedPromise({'message': model + ' does not contain field: id_' + referenceName});
+					}
+				}
+				// check if the reference exists in model reference name
+				return this.doesExist(id)
+				.then(function(exists){
+					if(exists){
+						return SchemaModel[referenceName].doesExist(referenceValue);
+					}else{
+						throw({'message': model + ' does not contain id: ' + id});
+					}
+				})
+				.then(function(exists){
+					if(exists){
+						referenceName = 'id_' + referenceName;
+						return setReference(referenceValue, id, referenceName);
+					}else{
+						throw({'message': referenceName + ' does not contain id: ' + referenceValue});
+					}
+				});
+			},
+			'addReference': function(id, referenceName, referenceValue) {
+				for (var item in schema[model].__refs__) {
+					var hasRef = false;
+					if (item === referenceName) {
+						hasRef = true;
+					}
+					if(!hasRef){
+						return rejectedPromise({'message': model + ' does not contain field: ids_' + referenceName});
+					}
+				}
+				// check if the reference exists in model reference name
+				return this.doesExist(id)
+				.then(function(exists){
+					if(exists){
+						return SchemaModel[referenceName].doesExist(referenceValue);
+					}else{
+						throw({'message': model + ' does not contain id: ' + id});
+					}
+				})
+				.then(function(exists){
+					referenceName = 'ids_' + referenceName;
+					if(exists){
+						referenceName = 'ids_' + referenceName;
+						return addReference(true, id, referenceName, referenceValue);
+					}else{
+						throw({'message': referenceName + ' does not contain id: ' + referenceValue});
+					}
+				});
+			},
+			'removeReference': function(id, referenceName, referenceValue) {
+				for (var item in schema[model].__refs__) {
+					var hasRef = false;
+					if (item === referenceName) {
+						hasRef = true;
+					}
+					if(!hasRef){
+						return rejectedPromise({'message': model + ' does not contain field: ids_' + referenceName});
+					}
+				}
+				// check if the reference exists in model reference name
+				return this.doesExist(id)
+				.then(function(exists){
+					referenceName = 'ids_' + referenceName;
+					if(exists){
+						referenceName = 'ids_' + referenceName;
+						return removeReference(id, referenceName, referenceValue);
+					}else{
+						throw({'message': model + ' does not contain id: ' + id});
+					}
+				});
+				
+			}
+		};
+	}
+
+};
+
+function rejectedPromise(error) {
+	var deffered = q.defer();
+	setTimeout(function() {
+		deffered.reject(error);
+	});
+	return deffered.promise;
+}
+
+function clone(obj) {
+	if (null === obj || "object" != typeof obj) return obj;
+	var copy = obj.constructor();
+	for (var attr in obj) {
+		if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+	}
+	return copy;
+}
---
+++ test/tests.js
@@ -0,0 +1,322 @@
+var testSchema = require('./testSchema.json');
+var firebaseModels = require('../');
+var Firebase = require('Firebase');
+
+var testURL = "https://tttests.firebaseio.com/";
+var testFirebaseSecret = "cI2lwD7QVtvRLTn8gGuBGawbKHdN0NYTHQg3rgYv";
+var rootRef = new Firebase(testURL);
+
+test = {};
+
+test.setTestDb = {
+	'set': function(test){
+		rootRef.set({}, function(err){
+			if(!err){
+				test.done();
+			}else{
+				throw new Error('error pushing test db');
+			}
+		});
+	}
+};
+
+test.schemaValidation = {
+	'bad reference id_': function(test){
+		try{
+			console.log('ersadfasdfasdf');
+			firebaseModels({
+				"users": {
+					"id_people": ""
+				}
+			});
+			FAILTEST(test, 'should not be here');
+		} catch(err){
+			test.done();
+		}
+	},
+	'bad reference ids_': function(test){
+		try{
+			firebaseModels({
+				"users": {
+					"ids_people": ""
+				}
+			});
+			FAILTEST(test, 'should not be here');
+		} catch(err){
+			test.done();
+		}
+	},
+	'validSchema': function(test){
+		try{
+			firebaseModels(testSchema, testURL, testFirebaseSecret);
+			test.done();
+		} catch(err){
+			console.log(err);
+			FAILTEST(test, 'should not be here');	
+		}
+	},
+};
+
+var SchemaModel = firebaseModels(testSchema, testURL, testFirebaseSecret);
+
+var addedUserId = null;
+var addedTeamId = null;
+var addedEmailsId = null;
+test.push = {
+	'success': function(test){
+		var user = {
+			'name': 'bob'
+		};
+		SchemaModel.users.push(user)
+		.then(function(successObj){
+			addedUserId = successObj;
+			test.done();
+		}).catch(function(error){
+			console.log(error);
+			FAILTEST(test, 'should not reject');
+		});
+	},
+	'invalid': function(test){
+		var user = {
+			'emailAddressss': 'bob'
+		};
+		SchemaModel.users.push(user)
+		.then(function(successObj){
+			FAILTEST(test, 'should not resolve');
+		}).catch(function(error){
+			test.done();
+		});
+	},
+	'add team': function(test){
+		var user = {
+			'name': 'teamname'
+		};
+		SchemaModel.teams.push(user)
+		.then(function(successObj){
+			addedTeamId = successObj;
+			test.done();
+		}).catch(function(error){
+			console.log(error);
+			FAILTEST(test, 'should not reject');
+		});
+	},
+	'add email': function(test){
+		var email = {
+			'email': 'teamname'
+		};
+		SchemaModel.emails.push(email)
+		.then(function(successObj){
+			addedEmailsId = successObj;
+			test.done();
+		}).catch(function(error){
+			console.log(error);
+			FAILTEST(test, 'should not reject');
+		});
+	}
+};
+
+test.add = {
+	'success': function(test){
+		var user = {
+			'name': 'bob'
+		};
+		SchemaModel.users.add('idetoadddude', user)
+		.then(function(successObj){
+			test.done();
+		}).catch(function(error){
+			console.log(error);
+			FAILTEST(test, 'should not reject');
+		});
+	},
+	'invalid': function(test){
+		var user = {
+			'emailAddressss': 'bob'
+		};
+		SchemaModel.users.add('shouldnotseethis', user)
+		.then(function(successObj){
+			FAILTEST(test, 'should not resolve');
+		}).catch(function(error){
+			test.done();
+		});
+	}
+};
+
+test.doesExist = {
+	'yes it does': function(test){
+		SchemaModel.users.doesExist(addedUserId)
+		.then(function(successObj){
+			test.equal(successObj, true);
+			test.done();
+		}).catch(function(error){
+			FAILTEST(test, 'should not reject');
+		});
+	},
+	'no it does not': function(test){
+		SchemaModel.users.doesExist('bullishId')
+		.then(function(successObj){
+			test.equal(successObj, false);
+			test.done();
+		}).catch(function(error){
+			FAILTEST(test, 'should not reject');
+		});
+	}
+};
+
+test.update = {
+	'success': function(test){
+		var user = {
+			'name': 'frank'
+		};
+		SchemaModel.users.update('idetoadddude', user)
+		.then(function(successObj){
+			test.done();
+		}).catch(function(error){
+			console.log(error);
+			FAILTEST(test, 'should not reject');
+		});
+	},
+	'invalid': function(test){
+		var user = {
+			'emailAddressss': 'bob'
+		};
+		SchemaModel.users.update('shouldnotseethis', user)
+		.then(function(successObj){
+			FAILTEST(test, 'should not resolve');
+		}).catch(function(error){
+			test.done();
+		});
+	},
+	'does not exist': function(test){
+		var user = {
+			'name': 'jane'
+		};
+		SchemaModel.users.update('shouldnotseethis', user)
+		.then(function(successObj){
+			FAILTEST(test, 'should not resolve');
+		}).catch(function(error){
+			test.done();
+		});
+	}
+};
+
+test.setReference = {
+	'success': function(test){
+		SchemaModel.users.setReference(addedUserId, 'emails', addedEmailsId)
+		.then(function(successObj){
+			test.done();
+		}).catch(function(error){
+			console.log(error);
+			FAILTEST(test, 'should not reject');
+		});
+	},
+	'secondary referecne does not exist': function(test){
+		SchemaModel.users.setReference(addedUserId, 'emails', 'asdfasdfasdfa fasdf')
+		.then(function(successObj){
+			FAILTEST(test, 'should not resolve');
+		}).catch(function(error){
+			test.done();
+		});
+	},
+	'primary index does not exiset': function(test){
+		SchemaModel.users.setReference('asdfasdfasdfasdf', 'emails', addedEmailsId)
+		.then(function(successObj){
+			FAILTEST(test, 'should not resolve');
+		}).catch(function(error){
+			test.done();
+		});
+	}
+};
+
+test.addReference = {
+	'success': function(test){
+		SchemaModel.users.addReference(addedUserId, 'teams', addedTeamId)
+		.then(function(successObj){
+			test.done();
+		}).catch(function(error){
+			console.log(error);
+			FAILTEST(test, 'should not reject');
+		});
+	},
+	'secondary referecne does not exist': function(test){
+		SchemaModel.users.addReference(addedUserId, 'teams', 'asdfasdfasdfa fasdf')
+		.then(function(successObj){
+			FAILTEST(test, 'should not resolve');
+		}).catch(function(error){
+			test.done();
+		});
+	},
+	'primary index does not exiset': function(test){
+		SchemaModel.users.addReference('asdfasdfasdfasdf', 'teams', addedEmailsId)
+		.then(function(successObj){
+			FAILTEST(test, 'should not resolve');
+		}).catch(function(error){
+			test.done();
+		});
+	}
+};
+
+test.removeReference = {
+	'primary index does not exist': function(test){
+		SchemaModel.users.removeReference('asdfasdfasdf', 'teams', 'asdfasdfasdfa fasdf')
+		.then(function(successObj){
+			FAILTEST(test, 'should not resolve');
+		}).catch(function(error){
+			test.done();
+		});
+	},
+	'success': function(test){
+		SchemaModel.users.removeReference(addedUserId, 'teams', addedTeamId)
+		.then(function(successObj){
+			test.done();
+		}).catch(function(error){
+			console.log(error);
+			FAILTEST(test, 'should not reject');
+		});
+	},
+};
+
+/*
+test.addEmail = {
+	'adding new email for activated user': function(test){
+		firebaseWrapper.addEmail('activatedUser@live.com')
+		.then(function(successObj){
+			test.done();
+		}).catch(function(error){
+			FAILTEST(test, 'should not reject');
+		});
+	},
+	'adding new email for unactivated': function(test){
+		firebaseWrapper.addEmail('unactivatedUser@live.com')
+		.then(function(successObj){
+			test.done();
+		}).catch(function(error){
+			FAILTEST(test, 'should not reject');
+		});
+	},
+	'adding email that exists already': function(test){
+		firebaseWrapper.addEmail('activatedUser@live.com')
+		.then(function(successObj){
+			FAILTEST(test, 'should not resolve');
+		}).catch(function(error){
+			test.deepEqual(error, errors[0]);
+			test.done();
+		});
+	},
+};
+*/
+//run last test
+test.lastTest = function(test){
+    //do_clear_all_things_if_needed();
+    setTimeout(process.exit, 500); // exit in 500 milli-seconds    
+    test.done();
+} ;
+
+function FAILTEST(test, message){
+	if(message){
+		console.log(message);
+	}
+	test.equal(true, false);
+	test.done();
+}
+
+module.exports = test;
